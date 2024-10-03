928a6baa769b8590ab4417bac4b340c9b4a44185
Dj-Dago <jonathan.gonzalez05@uptc.edu.co>
2024-10-02 20:13:30 -0500

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Obtener los elementos del DOM
+const modal = document.getElementById("modalFormulario");
+const btnAbrir = document.getElementById("abrirFormulario");
+const spanCerrar = document.getElementsByClassName("cerrar")[0];
+
 // Validación de contraseñas
 const form = document.getElementById('registroForm');
 form.addEventListener('submit', function (event) {
@@ -9,3 +14,21 @@ form.addEventListener('submit', function (event) {
         event.preventDefault(); // Previene el envío del formulario
     }
 });
+
+
+// Abrir el modal cuando se hace clic en el botón
+btnAbrir.onclick = function () {
+    modal.style.display = "flex";  // Mostrar el modal con flexbox para centrarlo
+}
+
+// Cerrar el modal cuando se hace clic en el botón de cerrar
+spanCerrar.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Cerrar el modal cuando se hace clic fuera del contenido
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
